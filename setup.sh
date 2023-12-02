b984d458679b94314755c3bea2be7f88af3de6b3
Mohammad Alamin <102026469+TeamBlackBerry@users.noreply.github.com>
2023-12-02 11:46:49 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ pkg update && pkg upgrade
 pkg install python3
 pkg install wget
 pip install -r requirements.txt
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install ./google-chrome-stable_current_amd64.deb
